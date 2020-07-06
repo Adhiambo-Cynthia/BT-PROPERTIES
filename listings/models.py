9c7fc4591789b684a649b99972888b468a4c51ee
Adhiambo_C <cinadhiambo22@gmail.com>
2020-07-06 15:12:47 +0300

    listing model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtors
 
-# Create your models here.
+class Listing(models.Model):
+    realtor=models.ForeignKey(
+        Realtors,
+        on_delete=models.DO_NOTHING,
+    )
+    title=models.CharField(max_length=200)
+    address=models.CharField(max_length=200)
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+    zipcode=models.CharField(max_length=20)
+    description=models.TextField(blank=True)
+    price=models.IntegerField()
+    bedrooms=models.IntegerField()
+    bathrooms=models.IntegerField()
+    garage=models.IntegerField(default=0)
+    sqft=models.IntegerField()
+    lot_size=models.DecimalField(max_digits=5, max_decimals=1)
+    photo_main=models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo1=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo2=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo3=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo4=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo5=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo6=models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published=models.BooleanField(default=True)
+    list_date=models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.title
+    
+    
+    
