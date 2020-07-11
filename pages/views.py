46cd464f5a5f1c439d7f0e5f6f77074cc4bd0f2b
Adhiambo_C <cinadhiambo22@gmail.com>
2020-07-11 17:18:44 +0300

    single listing

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtors
 
 def index(request):
-    return render(request, "pages/index.html")
+    listings=Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context={
+        'listings':listings
+    }
+    return render(request, "pages/index.html", context)
 
 def about(request):
-    return render(request, "pages/about.html")
+    # all realtors
+    realtors=Realtors.objects.order_by('-hire_date')
+    # seller of month
+    sellerofmonth=Realtors.objects.all().filter(seller_of_month=True)
+    context={
+        'realtors':realtors,
+        'sellerofmonth':sellerofmonth
+    }
+    return render(request, "pages/about.html", context)
