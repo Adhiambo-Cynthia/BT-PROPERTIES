3b992ab77c6bb4068344187213fb48bc686031ae
Adhiambo_C <cinadhiambo22@gmail.com>
2020-07-12 17:25:40 +0300

    accounts app and urls

--- web_project/urls.py
+++ web_project/urls.py
@@ -22,5 +22,6 @@ admin.site.site_title = 'BTProperties admin'
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
