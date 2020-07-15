95a1cb8d3f92e5e0fbf6aac8fbcde6c402e345c4
Adhiambo_C <cinadhiambo22@gmail.com>
2020-07-15 20:00:47 +0300

    contact form fields and admin customization

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display=('id', 'name', 'email', 'phone', 'contact_date')
+    list_display_links=('id', 'name')
+    list_filter=('contact_date',)
+    search_fields=('listing','email', 'name')
+    list_per_page= 20
+
+
+admin.site.register(Contact, ContactAdmin)
+
+
