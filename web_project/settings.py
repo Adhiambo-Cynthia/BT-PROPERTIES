2b6566f059dc04ca9ca4788308616d6701c24305
Adhiambo_C <cinadhiambo22@gmail.com>
2020-07-17 20:25:32 +0300

    deploy

--- web_project/settings.py
+++ web_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://btpropertiesapp.herokuapp.com/']
+ALLOWED_HOSTS = ['https://btpropertiesapp.herokuapp.com']
 
 
 # Application definition
