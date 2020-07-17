23bb07418901296958686eaf3d0f2887576bb46f
Adhiambo_C <cinadhiambo22@gmail.com>
2020-07-17 16:05:01 +0300

    files and changes required for deployment to heroku

--- web_project/settings.py
+++ web_project/settings.py
@@ -136,10 +136,8 @@ MESSAGE_TAGS = {
 #email config
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='from@gmail.com'
-EMAIL_HOST_PASSWORD= 'your gmail password or app password if you have two step verification on'
+EMAIL_HOST_USER='cinadhiambo22@gmail.com'
+EMAIL_HOST_PASSWORD= 'brhwrioujnhujwgp'
 EMAIL_USE_TLS=True
-
-
 
 
